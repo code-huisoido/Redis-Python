f929bb203518f7caa7d5f2d4a2a58362a810c311
huisoido <584331125@qq.com>
2017-11-18 20:48:26 +0800

    docs: redis的append、substr、setrange、setbit操作

---
+++ 3/string/redis-string2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import redis
+r = redis.Redis(host='localhost', port=6379)
+r.append('new-string-key', 'hello ')
+r.append('new-string-key', 'world!')
+print r.get('new-string-key')
+
+# 截取字符串
+print r.substr('new-string-key', 3, 7)
+
+# 替换字符串
+r.setrange('new-string-key', 0, 'H')
+r.setrange('new-string-key', 6, 'W')
+print r.get('new-string-key')
+
+# 扩展字符串
+r.setrange('new-string-key', 11, ', how are you?')
+print r.get('new-string-key')
+
+# 设置二进制位串 00100001 => 33
+r.setbit('another-key', 2, 1)
+r.setbit('another-key', 7, 1)
+print r.get('another-key')
